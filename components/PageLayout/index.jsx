0ac99bb0abf26e003c90c30865a132a17d6958cf
DEEPESH S <deepesh@madon.io>
2022-07-29 17:58:35 +0530

    fix: removed google tag manager scripts

--- components/PageLayout/index.jsx
+++ components/PageLayout/index.jsx
@@ -8,14 +8,6 @@ const PageLayout = ({ menuData, yoastSeo, schema, seo, classNames = '', children
   return (
     <>
       <Head yoastSeo={yoastSeo} schema={schema} seo={seo} />
-      <noscript
-        dangerouslySetInnerHTML={{
-          __html: `
-        <iframe src="https://www.googletagmanager.com/ns.html?id=GTM-T5XNDHQ"
-        height="0" width="0" style="display:none;visibility:hidden"></iframe>
-        `,
-        }}
-      ></noscript>
       <HeaderMobile menuData={menuData} />
       <Header menuData={menuData} />
       <main className={`page-layout ${classNames}`}>{children}</main>
